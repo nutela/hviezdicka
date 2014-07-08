7f60dc818776dd52e6c3f3fe451bda6a199af42f
Jana Staurovska <nutela.sabe@gmail.com>
2014-07-08 18:24:45 +0200

    kozmeticke zmeny

--- hviezdicka.py
+++ hviezdicka.py
@@ -4,19 +4,22 @@ Usage: hviezdicka.py (add | remove) --token=TOKEN <username> <reponame>
 
 """
 
-import requests
 import pprint
 import sys
+
+import requests
 import docopt
 
-options=docopt.docopt(__doc__)
-token = options["--token"]
+options = docopt.docopt(__doc__)
 
+token = options["--token"]
 headers = {'Authorization': 'token {}'.format(token)}
 username = options["<username>"]
 reponame = options["<reponame>"]
 url = 'https://api.github.com/user/starred/{}/{}'.format(username, reponame)
+
 if options["add"]:
     r = requests.put(url, headers=headers)
 else:
     r = requests.delete(url, headers=headers)
+
