1ae38b8663e9213ae5f3dfda65d0674df3e7b303
Jana Staurovska <nutela.sabe@gmail.com>
2014-07-08 18:17:43 +0200

    pridavanie a odoberanie hviezdicky na githube

--- hviezdicka.py
+++ hviezdicka.py
@@ -1,9 +1,22 @@
+"""Pridava a odobera hviezdicky na githube
+
+Usage: hviezdicka.py (add | remove) --token=TOKEN <username> <reponame>
+
+"""
+
 import requests
 import pprint
 import sys
+import docopt
 
-token = sys.argv[1]
+options=docopt.docopt(__doc__)
+token = options["--token"]
+
 headers = {'Authorization': 'token {}'.format(token)}
-r = requests.get('https://api.github.com', headers=headers)
-pprint.pprint(r.json())
-
+username = options["<username>"]
+reponame = options["<reponame>"]
+url = 'https://api.github.com/user/starred/{}/{}'.format(username, reponame)
+if options["add"]:
+    r = requests.put(url, headers=headers)
+else:
+    r = requests.delete(url, headers=headers)
